02910561e4b8cac217cf3788cd9983cbebf157ac
ShrushtiP17 <shrushtiphatate37@gmail.com>
2024-10-11 11:33:22 +0530

    Add files via upload

---
+++ shrushti2.py
@@ -0,0 +1,16 @@
+# x='"Avengers Assemble"-Cap.America'
+# print(x)
+# y="\"Avengers Assemble\"-\"Cap.America\""
+# print(y)
+# #type casting
+# var1="10"
+# print(float("10.0"))
+num1=int(input("Enter Number 1:"))
+print(num1)
+num2=int(input("Enter Number 2:"))
+# print(num2)
+# print(num1+num2)
+print("Add:",num1+num2)
+print("Sub:",num1-num2)
+print("Mul:",num1*num2)
+print("Div:",num1/num2)
