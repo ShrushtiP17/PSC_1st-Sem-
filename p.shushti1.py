36fcef6d73ba46e027bbc2aefd081c247c46adf3
ShrushtiP17 <shrushtiphatate37@gmail.com>
2024-10-23 11:31:14 +0530

    Add files via upload

---
+++ p.shushti1.py
@@ -0,0 +1,26 @@
+#ARITHMETIC OPERATORS
+a=10
+b=5
+print("addition:",a+b)
+print("substraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor_division:",a//b)
+print("modulus:",a%b)
+print("exponent:",a**b)
+
+
+#Regional Operators
+print("a>b is",a>b)
+print("a<b is",a<b)
+print("a=b is",a==b)
+print("a!=b is",a!=b)
+print("a>=b is",a!=b)
+print("a>=b is",a>=b)
+
+
+#Logical Operators
+print("logical operator")
+print(10<5 and 10<20)
+print(10<5 or 10<20)
+print(not(10<20))
