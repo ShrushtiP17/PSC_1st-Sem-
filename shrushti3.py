75089899c997f89013bb545ce289edbe60d26c42
ShrushtiP17 <shrushtiphatate37@gmail.com>
2024-10-23 16:59:04 +0530

    Add files via upload

--- shrushti3.py
+++ shrushti3.py
@@ -1,18 +1,38 @@
-# x='"Avengers Assemble"-Cap.America'
-# print(x)
-# y="\"Avengers Assemble\"-\"Cap.America\""
-# print(y)
-# #type casting
-# var1="10"
-# print(float("10.0"))
-num1=int(input("Enter Number 1:"))
-print(num1)
-num2=int(input("Enter Number 2:"))
-# print(num2)
-# print(num1+num2)
-print("Add:",num1+num2)
-print("Sub:",num1-num2)
-print("Mul:",num1*num2)
-print("Div:",num1/num2)
+temperature=int(input("Enter Temperature"))
+if temperature<40:
+     print("Cold")
+else:
+     print("Nice Weather")
 
 
+hours=int(input("Enter total working hours"))
+payroll=100
+ob=5
+if hours>40:
+    a=hours*payroll*ob
+    print("Overtime Pay",a)
+else:
+    b=hours*payroll
+    print("Usual Pay",b)
+
+x=5
+if x>10:
+    print("Bigger")
+else:
+    print("Smaller")
+
+print("Finish")
+
+x=5
+if x==5:
+    print("Equals to 5")
+if x>4:
+    print("Greater than 4")
+if x>=5:
+    print("Greater than equals to 5")
+if x<6:
+    print("lesser than 6")
+if x<=5:
+    print("Less than equals to 5")
+if x!=6:
+    print("Not equals to 6")
