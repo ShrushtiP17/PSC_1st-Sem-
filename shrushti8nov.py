227e1fc60f0820f81041d48ea15ebc906d864832
ShrushtiP17 <shrushtiphatate37@gmail.com>
2024-11-08 11:33:20 +0530

    Add files via upload

---
+++ shrushti8nov.py
@@ -0,0 +1,42 @@
+print("happy diwali")
+
+x=int(input("enter a number"))
+if x%4==0:
+    print("Yes")
+elif x%5.0==0:
+    print("Yes")
+else:
+    print("No")
+
+
+x="happy diwali"
+for str in range(100):
+    print(x)
+
+
+for num in range (1,101):
+    print(num)
+
+  
+for i in range (1,11):
+    print(i*i)
+i=1
+for i in range (200):
+    a=i*i
+    if a<200:
+     print(a)
+#Find first 10 and first 200 fabonacci numbers
+a=0
+b=1
+for int in range (10):
+    print(a)
+    a,b=b,a+b
+
+while a<200:
+    print(a)
+    a,b=b,a+b
+
+
+
+
+
