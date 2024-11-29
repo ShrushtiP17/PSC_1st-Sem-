d77094b9dd7261c92586a67dfbdda1ab35a6fc31
ShrushtiP17 <shrushtiphatate37@gmail.com>
2024-11-29 08:19:29 +0530

    Add files via upload

---
+++ shrushti22nov.py
@@ -0,0 +1,22 @@
+# x=int(input("enter age of the person"))
+# if x<18:
+#     print("Teenager")
+# if x>18 and x<30:
+#     print("Adult")
+# elif x>30:
+#     print("Senior")
+
+def age(age1):
+    if age1<18:
+        return"Teenager"
+    if age1>=18 and age1<=30:
+        return "Adult"
+    if age1>30:
+        return "Senior"
+x=int(input("enter age of the person"))
+y=int(input("enter age of the person"))
+z=int(input("enter age of the person"))
+
+print(age(x))
+print(age(y))
+print(age(z))
